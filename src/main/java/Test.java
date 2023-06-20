82feb27951b3c4a53a63053e916f1b5f8cf95830
Viner108 <andrukovichl@mail.ru>
2023-06-20 17:50:20 +0300

    searchInThisRoom commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,19 +5,19 @@ import room.Suitcase;
 import room.Table;
 
 public class Test {
-// id - категория предмета
+    // id - категория предмета
 //    1-техника
 //    2-одежда
 //    3-еда
 //    4-другое
     public static void main(String[] args) {
-        Item computer=new Item("Computer",1,1,1,1);
-        Item toy=new Item("Toy",4,2,2,2);
-        Item sweet=new Item("Sweet",3,0.01,0.05,0.01);
-        Item dress=new Item("Dress",2,0.01,1,1.5);
-        Table table=new Table(1,2,2);
-        Bed bed=new Bed(2,3,5);
-        Suitcase suitcase=new Suitcase(0.5,1,0.5);
+        Item computer = new Item("Computer", 1, 1, 1, 1);
+        Item toy = new Item("Toy", 4, 2, 2, 2);
+        Item sweet = new Item("Sweet", 3, 0.01, 0.05, 0.01);
+        Item dress = new Item("Dress", 2, 0.01, 1, 1.5);
+        Table table = new Table(1, 2, 2);
+        Bed bed = new Bed(2, 3, 5);
+        Suitcase suitcase = new Suitcase(0.5, 1, 0.5);
         table.insert(computer);
         table.insert(toy);
         table.insert(computer);
@@ -36,17 +36,29 @@ public class Test {
 //        table.movement(computer,bed);
 //        bed.remove(computer);
 //        bed.answerSearch(computer);
-        randomPlace(sweet,bed,table,suitcase);
-        System.out.println(bed.getWidth()+ " " +bed.getLength()+ " " +bed.getHeight());
-        System.out.println(table.getWidth()+ " "+ table.getLength()+ " "+ table.getHeight());
-        System.out.println(suitcase.getWidth()+" "+suitcase.getLength()+ " "+ suitcase.getHeight());
-        System.out.println(bed.volume());
-        System.out.println(table.volume());
+        randomPlace(sweet, bed, table, suitcase);
+        searchInThisRoom(computer, bed, suitcase, table);
+
+//        System.out.println(bed.getWidth()+ " " +bed.getLength()+ " " +bed.getHeight());
+//        System.out.println(table.getWidth()+ " "+ table.getLength()+ " "+ table.getHeight());
+//        System.out.println(suitcase.getWidth()+" "+suitcase.getLength()+ " "+ suitcase.getHeight());
+//        System.out.println(bed.volume());
+//        System.out.println(table.volume());
     }
-    public static void randomPlace(Item item, Place... places){
-        for (Place place:places){
-            if(place.volume()>item.volume()&&place.indexCheck(item)){
+
+    public static void randomPlace(Item item, Place... places) {
+        for (Place place : places) {
+            if (place.volume() > item.volume() && place.indexCheck(item)) {
                 place.insert(item);
+                break;
+            }
+        }
+    }
+
+    public static void searchInThisRoom(Item item, Place... places) {
+        for (Place place : places) {
+            if (place.search(item)) {
+                place.answerSearch(item);
                 break;
             }
         }
